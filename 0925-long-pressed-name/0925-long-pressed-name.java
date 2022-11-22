920dfff2f452dafc320b36384fd4aea163fce2e6
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2022-11-23 00:06:25 +0530

    Time: 1 ms (92.72%), Space: 42 MB (60.26%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isLongPressedName(String name, String typed) {
-     // if(name.length()!=typed.length()) return true;
+     // if(name.length()==typed.length()) return true;
       if(name.length()>typed.length())  return false;
         
         int i=0;
