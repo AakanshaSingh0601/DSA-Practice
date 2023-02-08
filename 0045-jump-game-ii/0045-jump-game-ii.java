ad9d3bf5acb844c0201561b0898d65fd331ae58a
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2023-02-08 22:57:02 +0530

    Time: 405 ms (5.12%), Space: 42.7 MB (64.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,9 +1,10 @@
 class Solution {
     public int jump(int[] nums) {
          int[] dp = new int[nums.length];
-        for(int i = 0; i<nums.length; i++)
+        for(int i = 0; i<nums.length; i++){
             dp[i] = Integer.MAX_VALUE;
             dp[0] = 0;
+        }
         for(int i = 1; i<nums.length; i++){
             for(int j = 0; j<i; j++){
                 if(dp[j]!=Integer.MAX_VALUE && j+nums[j]>=i)
