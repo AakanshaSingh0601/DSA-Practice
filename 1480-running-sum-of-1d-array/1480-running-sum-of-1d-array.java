3031b211d816c23f5c9000100c4230740a7b21ed
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2023-03-26 22:24:02 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (15.51%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -4,8 +4,7 @@ class Solution {
         ans[0]=nums[0];
         for(int i=1;i<nums.length;i++){
            ans[i]=nums[i]+ ans[i-1];
-            
-        }
+            }
         return ans;   
     }
 }
