9f10418e89e1e77585f7bdf83852af78e08e6d06
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2023-03-26 22:47:03 +0530

    Time: 1 ms (100.00%), Space: 43 MB (67.60%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,18 +1,25 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int left_Sum =0;
-        int total_Sum=0;
-        for(int i =0; i<nums.length; i++){
-            total_Sum += nums[i];
+     int n= nums.length;
+     int sumLeft[] = new int[n];
+     int sumRight[]= new int[n];
+        sumLeft[0] = nums[0];
+        for(int i=1;i<n;i++){
+            sumLeft[i]= nums[i] +sumLeft[i-1];
         }
-        for(int i =0; i<nums.length; i++){
-            if(i!=0) left_Sum += nums[i-1];
-            if(total_Sum -left_Sum - nums[i] == left_Sum){
+        sumRight[n-1]= nums[n-1];
+        for(int i=n-2; i>=0; i--){
+            sumRight[i]= nums[i]+ sumRight[i+1];
+        }
+        for(int i=0; i<n;i++){
+            if(sumLeft[i] == sumRight[i])
                 return i;
-            }
         }
-        return -1;
+       return -1; 
     }
 }
         
-    
+//
+// [1,7,3,6,5,6] 
+//lS[1,8,11,17,22,28]          
+//rs[28,27,20,17,11,6]
