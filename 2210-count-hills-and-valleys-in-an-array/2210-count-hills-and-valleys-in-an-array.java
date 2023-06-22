8672129d80e9bec295caa6798ef1fa38ec20a33b
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2023-06-22 14:37:41 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (71.52%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -1,14 +1,24 @@
 class Solution {
     public int countHillValley(int[] nums) {
-        int length = nums.length;
-        int countHillValley = 0;
-        int previous = nums[0];
-        for (int i = 1; i < length - 1; i++) {
-            if (previous < nums[i] && nums[i] > nums[i + 1] || previous > nums[i] && nums[i] < nums[i + 1]) {
-                previous = nums[i];
-                countHillValley++;
-            }
+        int count =0;
+        int prevs =nums[0];
+        
+        for(int i=1;i<nums.length-1;i++){
+               if(prevs<nums[i] && nums[i]>nums[i+1] || prevs>nums[i] && nums[i]<nums[i+1] ){
+                   prevs=nums[i];
+            count++;
         }
-        return countHillValley;
+    }
+        
+       return count; 
     }
 }
+
+
+//2 4 1 1 6 5
+// i-1 < i> j
+
+
+// if (previous < nums[i] && nums[i] > nums[i + 1] || previous > nums[i] && nums[i] < nums[i + 1]) {
+//                 previous = nums[i];
+//                 countHillValley++;
