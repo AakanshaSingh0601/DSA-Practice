047023d8c723aa736409efcf9ce7f61939a95cb6
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2023-08-14 23:37:22 +0530

    Time: 4 ms (64.79%), Space: 41.8 MB (60.75%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String str[]= title.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(String s : str ){
+            if(s.length()<3) {
+               sb.append(s.toLowerCase());
+            }
+            else{
+                String word= s.substring(0,1).toUpperCase() + s.substring(1).toLowerCase();
+                sb.append(word);
+            }
+            sb.append(" ");
+            }
+       sb.deleteCharAt(sb.length()-1);
+      return sb.toString();  
+    }
+}
+
+
