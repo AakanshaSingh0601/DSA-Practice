4212788203613083706d5500d6d46cc326409ab2
Aakansha Singh <100512376+AakanshaSingh0601@users.noreply.github.com>
2023-08-23 23:58:10 +0530

    Time: 13 ms (77.42%), Space: 47.5 MB (81.21%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int ans[] = new int[k];
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i: nums ){
+       HashMap<Integer,Integer>  map = new HashMap<>();
+        for(int i: nums){
             map.put(i,map.getOrDefault(i,0)+1);
-        }
-        int size = map.size();
-        int [][] arr = new int[size] [2];
-        int idx= 0;
-        for(int key: map.keySet()){
-            arr[idx][0] = key;
-            arr[idx][1] = map.get(key);
-            idx++;
-        }
+            }
         
-        Arrays.sort(arr, (a,b) -> b[1] - a[1]);
-        for(int i=0;i<k;i++){
-            ans[i] = arr[i][0];
-        }
-            
+        int n = map.size();
+        int grid[] [] = new int[n][2];
+        int idx=0;
+        for(int key : map.keySet()){
+             grid[idx][0] = key;
+             grid[idx][1]= map.get(key);
+            idx++;
+            }
+        Arrays.sort(grid, (a,b) -> b[1]- a[1]);
+        int ans[] = new int[k];
+        
+        for(int i =0;i<k;i++){
+            ans[i]= grid[i][0];
+            }
         return ans;
     }
 }
